004caa86faa54945719f3f1dd68abeeaa809baf1
Andriamanaja <r.andriamanaja@gmail.com>
2019-09-14 14:32:11 +0300

    test4

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/inscae'));
+app.use(express.static(__dirname + '/dist/fronton'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/inscae/index.html'));
+res.sendFile(path.join(__dirname+'/dist/fronton/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
